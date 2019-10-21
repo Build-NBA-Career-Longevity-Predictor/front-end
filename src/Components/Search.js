33539903abfcac90320713330391b9f1960c3647
jambis <imjaamees@gmail.com>
2019-10-21 16:06:44 -0500

    Search component filled

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,10 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+//import { fetchPlayerList, searchPlayer } from "../actions"; //importing actions to populate react-select and to search player
+import Select from "react-select";
 
 const Search = () => {
-  //Search Form here
-  //Maybe react-select to populate an dropdown input with all the players from existing database
-  //Maybe chance to input your own player who's currently not in the database (will need to be able to input stats)
-  return <div>Search</div>;
+  const dispatch = useDispatch();
+  //const playerList = useSelector(state => state.playerList) //grab playerlist from our global state
+  const playerList = ["Michael Jordan", "LeBron James", "Kobe Bryant"];
+
+  const [selectedOption, setSelectedOption] = useState(null);
+
+  //Get player list when component renders
+  // useEffect(() => {
+  //   dispatch(fetchPlayerList());
+  // }, [dispatch]);
+
+  //Once player selects a player go to DS backend to get stats
+  // useEffect(() => {
+  //   if (playerList && selectedOption) {
+
+  // }, [selectedOption, playerList, dispatch]);
+
+  const options = playerList.map(item => ({
+    value: item,
+    label: item
+  }));
+
+  const handleChange = e => {
+    setSelectedOption(e);
+  };
+
+  return (
+    <div style={{ color: "#000" }}>
+      <Select
+        value={selectedOption}
+        onChange={e => handleChange(e)}
+        options={options}
+      />
+    </div>
+  );
 };
 
 export default Search;
