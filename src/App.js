3e3ae4775d608812e0d3cfdfb3fe4df7e83b81f1
Colin Bazzano <colinbazzano@Colins-MacBook-Pro.local>
2019-10-21 14:06:50 -0400

    created routes for the Register and Login components and properly imported those and routed them accordingly

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+//local imports below
+import Register from './Components/Register';
+import Login from './Components/Login';
 
 function App() {
   //Everything related to react routes here
-  return <div className="App">Hello World</div>;
+  return (
+  <div className="App">
+    <Router>
+    Hello World
+    <Route path="/" component={Register} />
+    <Route path="/login" component={Login} />
+    </Router>
+  </div>
+  );
 }
 
 export default App;
