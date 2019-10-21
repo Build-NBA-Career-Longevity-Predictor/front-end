b583e31c68ca4489839119f9f0460dde86395786
Colin Bazzano <colinbazzano@Colins-MacBook-Pro.local>
2019-10-21 15:40:57 -0400

    Populated the navbar with 3 current links as well as imported and deployed it in the App file and changed single quotes in imports to be double quotes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 //local imports below
-import Register from './Components/Register';
-import Login from './Components/Login';
-import PlayerList from './Components/PlayerList';
+import Register from "./Components/Register";
+import Login from "./Components/Login";
+import PlayerList from "./Components/PlayerList";
+import Navbar from "./Components/Navbar";
 
 function App() {
   //Everything related to react routes here
   return (
   <div className="App">
     <Router>
-    Hello World
-    <Route path="/" component={Register} />
-    <Route path="/login" component={Login} />
-    <Route path="/playerlist" component={PlayerList} />
+      <Navbar />
+      Hello World
+      <Route path="/" component={Register} />
+      <Route path="/login" component={Login} />
+      <Route path="/playerlist" component={PlayerList} />
     </Router>
   </div>
   );
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,10 +1,17 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   //Navbar menu here
   //Able to see login and register if not logged in
   //Can see search for player and saved players once logged in
-  return <div>Navbar</div>;
+  return (
+  <div className="Navbar-container">
+    <Link to="/">Register</Link>
+    <Link to="/login">Log In</Link>
+    <Link to="/playerlist">Players</Link>
+  </div>
+  );
 };
 
 export default Navbar;
