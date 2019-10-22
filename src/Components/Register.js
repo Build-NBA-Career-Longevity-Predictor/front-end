b98dbb1be63b1db9b914bb6714a8a3bf3efd73bc
jambis <imjaamees@gmail.com>
2019-10-22 13:55:41 -0500

    Completed Styling on Register Component

--- src/Components/Register.js
+++ src/Components/Register.js
@@ -1,5 +1,69 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
+import EmailIcon from "@material-ui/icons/Email";
+import LockIcon from "@material-ui/icons/Lock";
+import PersonIcon from "@material-ui/icons/Person";
+import { makeStyles } from "@material-ui/core/styles";
 import axios from "axios";
+import registerbg from "../Assests/Images/registerbg.jpg";
+
+const useStyles = makeStyles(theme => ({
+  container: {
+    display: "flex"
+  },
+  imgContainer: {
+    width: "70%",
+    height: "100vh"
+  },
+  img: {
+    width: "100%",
+    height: "100%",
+    filter: "grayscale(100%)"
+  },
+  formContainer: {
+    background: "#17408B",
+    width: "50%",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    paddingTop: "10%"
+  },
+  Form: {
+    background: "white",
+    borderRadius: "5px",
+    boxShadow: "2px 4px 5px #000",
+    padding: "30px 50px",
+    fontFamily: "Lato, sans-serif"
+  },
+  TextField: {
+    width: 400,
+    margin: "5px 0"
+  },
+  Input: {
+    fontSize: "24px",
+    fontFamily: "Lato, sans-serif"
+  },
+  Label: {
+    fontSize: "18px",
+    fontFamily: "Lato, sans-serif"
+  },
+  Icon: {
+    //
+  },
+  Button: {
+    width: "440px",
+    margin: "10px 0",
+    fontSize: "18px",
+    fontFamily: "Lato, sans-serif"
+  },
+  Header: {
+    fontFamily: "Oswald, sans-serif",
+    color: "white"
+  }
+}));
 
 const Register = () => {
   const [newRegister, setNewRegister] = useState({
@@ -7,6 +71,7 @@ const Register = () => {
     password: "",
     email: ""
   });
+  const classes = useStyles();
 
   const changeHandler = event => {
     setNewRegister({
@@ -17,6 +82,7 @@ const Register = () => {
 
   const handleSubmit = event => {
     event.preventDefault();
+    console.log("ran");
     axios
       .post(`https://nbapredictor-backend.herokuapp.com/signup`, newRegister)
       .then(res => {
@@ -24,45 +90,97 @@ const Register = () => {
         localStorage.setItem("token_type", res.data.token_type);
         localStorage.setItem("expires_in", res.data.expires_in);
       })
-      .catch(error => console.log(error.response));
+      .catch(error => console.error(error));
 
     setNewRegister({ username: "", password: "", email: "" });
   };
-  return (
-    <div className="register-main">
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="username"></label>
-        <input
-          name="username"
-          id="username"
-          type="text"
-          placeholder="Name"
-          onChange={changeHandler}
-          value={newRegister.username}
-        />
-        <label htmlFor="password"></label>
-        <input
-          name="password"
-          id="password"
-          type="text"
-          placeholder="Password"
-          onChange={changeHandler}
-          value={newRegister.password}
-        />
-        <label htmlFor="email"></label>
-        <input
-          name="email"
-          id="email"
-          type="text"
-          placeholder="Email"
-          onChange={changeHandler}
-          value={newRegister.email}
-        />
 
-        <button className="button" type="submit">
-          Sign Up
-        </button>
-      </form>
+  return (
+    <div className={classes.container}>
+      <div className={classes.imgContainer}>
+        <img
+          className={classes.img}
+          src={registerbg}
+          alt="Basketball in a court"
+        />
+      </div>
+      <div className={classes.formContainer}>
+        <h1 className={classes.Header}>Start getting predictions now!</h1>
+        <form className={classes.Form} onSubmit={handleSubmit}>
+          <div>
+            <Grid container spacing={1} alignItems="flex-end">
+              <Grid item style={{ paddingBottom: "18px" }}>
+                <PersonIcon className={classes.Icon} />
+              </Grid>
+              <Grid item>
+                <TextField
+                  id="username"
+                  label="Username"
+                  name="username"
+                  type="text"
+                  shrink="false"
+                  className={classes.TextField}
+                  onChange={changeHandler}
+                  value={newRegister.username}
+                  InputProps={{ className: classes.Input }}
+                  InputLabelProps={{ className: classes.Label }}
+                />
+              </Grid>
+            </Grid>
+          </div>
+          <div>
+            <Grid container spacing={1} alignItems="flex-end">
+              <Grid item style={{ paddingBottom: "18px" }}>
+                <LockIcon />
+              </Grid>
+              <Grid item>
+                <TextField
+                  id="password"
+                  label="Password"
+                  name="password"
+                  type="password"
+                  onChange={changeHandler}
+                  value={newRegister.password}
+                  className={classes.TextField}
+                  InputProps={{ className: classes.Input }}
+                  InputLabelProps={{ className: classes.Label }}
+                />
+              </Grid>
+            </Grid>
+          </div>
+          <div>
+            <Grid container spacing={1} alignItems="flex-end">
+              <Grid item style={{ paddingBottom: "18px" }}>
+                <EmailIcon />
+              </Grid>
+              <Grid item>
+                <TextField
+                  id="email"
+                  label="Email"
+                  name="email"
+                  type="email"
+                  onChange={changeHandler}
+                  value={newRegister.email}
+                  className={classes.TextField}
+                  InputProps={{ className: classes.Input }}
+                  InputLabelProps={{ className: classes.Label }}
+                />
+              </Grid>
+            </Grid>
+          </div>
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+            className={classes.Button}
+          >
+            Sign Up!
+          </Button>
+          <p>
+            Already have an account? <Link to="/login">Log in</Link>
+          </p>
+        </form>
+      </div>
     </div>
   );
 };
