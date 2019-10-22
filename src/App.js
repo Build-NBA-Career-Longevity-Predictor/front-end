8d5a3a69cd98949dbcbab61432f08c39c60ea37f
Colin Bazzano <colinbazzano@Colins-MacBook-Pro.local>
2019-10-22 15:21:34 -0400

    finished styling the stats card and still contains hardcoded values and I have removed it from the app file until we know where it will go as to prevent any merge conflicts

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
-import PlayerList from "./Components/PlayerList";
 import "./App.css";
 
 function App() {
   //Everything related to react routes here
   return (
     <div>
-      <PlayerList />
     </div>
   )
 }
