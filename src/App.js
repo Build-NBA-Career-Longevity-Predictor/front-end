6523e3b2d6e0fb0468a4690dd9120714a91657c3
Colin Bazzano <colinbazzano@Colins-MacBook-Pro.local>
2019-10-22 15:52:09 -0400

    adding dependencies

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
+
 //local imports below
 import Register from "./Components/Register";
 import Login from "./Components/Login";
