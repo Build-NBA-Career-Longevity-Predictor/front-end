54f31563bc9d52ce592744da147200d76bdd3006
James Bishop <imjaamees@gmail.com>
2019-10-22 14:33:48 -0500

    Updated App.js with what's current

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from "react";
-import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+//local imports below
+import Register from "./Components/Register";
+import Login from "./Components/Login";
+import PlayerList from "./Components/PlayerList";
 
 function App() {
   //Everything related to react routes here
   return (
-    <div>
+    <div className="App">
+      <Router>
+        <Route exact path="/" component={Register} />
+        <Route path="/login" component={Login} />
+        <Route path="/playerlist" component={PlayerList} />
+      </Router>
     </div>
-  )
+  );
 }
 
 export default App;
