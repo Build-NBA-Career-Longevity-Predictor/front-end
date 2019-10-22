cfd8013d67a417deba7e63db3a56be3b6d0c5327
Colin Bazzano <colinbazzano@Colins-MacBook-Pro.local>
2019-10-22 13:03:53 -0400

    created the containers and card containers for the playerlist

--- src/Components/PlayerList.js
+++ src/Components/PlayerList.js
@@ -2,7 +2,16 @@ import React from "react";
 
 const PlayerList = () => {
   //Map over saved playerlist state and return a Player component for each player
-  return <div>PlayerList</div>;
+  return (
+    <div className='player-container'>
+      <div className='player-cards'>
+        <div className='searched-player'></div>
+        <div className='stats-card'></div>
+        <div className='similar-player'></div>
+      </div>
+      <div className='expected-years'></div>
+    </div>
+  )
 };
 
 export default PlayerList;
