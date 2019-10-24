0c7885336ff07b3ab297d51fcc316c5a9808ba63
Colin Bazzano <colinbazzano@Colins-MacBook-Pro.local>
2019-10-24 12:26:37 -0400

    added dynamic data as well as styled the search for a player upon initial search

--- src/Components/PlayerList.js
+++ src/Components/PlayerList.js
@@ -5,6 +5,7 @@ import SavedPlayer from "./SavedPlayer";
 
 const PlayerList = () => {
   const state = useSelector(state => state.playersReducer);
+  console.log('THIS RIGHT HERE', state);
 
   const renderStats = () => {
     return (
@@ -17,45 +18,45 @@ const PlayerList = () => {
         <div className="player-cards">
           <div className="searched-player">
             <h2>{state.currentPlayer.name}</h2>
-            <p>IMAGE HERE</p>
-            <p>PG</p>
-            <p>6'3"</p>
-            <p>190lbs</p>
-            <p>College: Davidson</p>
+            <img src={state.currentPlayer.imgurl} alt="current player"></img>
+            <p>{state.currentPlayer.position}</p>
+            <p>{state.currentPlayer.height}</p>
+            <p>{state.currentPlayer.weight}</p>
+            <p>College: {state.currentPlayer.college}</p>
           </div>
           <div className="stats-card">
             <div className="points">
-              <p>26</p>
+              <p>{state.currentPlayer.pts_pg}</p>
               <h3 className="stats">POINTS PER GAME</h3>
-              <p>24</p>
+              <p>{state.currentPlayer.similarplayers[0].pts_pg}</p>
             </div>
             <div className="assists">
-              <p>5</p>
+              <p>{state.currentPlayer.assists_pg}</p>
               <h3 className="stats">ASSISTS PER GAME</h3>
-              <p>2</p>
+              <p>{state.currentPlayer.similarplayers[0].assists_pg}</p>
             </div>
             <div className="rebounds">
-              <p>8</p>
+              <p>{state.currentPlayer.rebounds_pg}</p>
               <h3 className="stats">REBOUNDS PER GAME</h3>
-              <p>10</p>
+              <p>{state.currentPlayer.similarplayers[0].rebounds_pg}</p>
             </div>
             <div className="minutes">
-              <p>37</p>
+              <p>{state.currentPlayer.mins_pg}</p>
               <h3 className="stats">MINUTES PER GAME</h3>
-              <p>36</p>
+              <p>{state.currentPlayer.similarplayers[0].mins_pg}</p>
             </div>
           </div>
           <div className="similar-player">
             <h2>{state.currentPlayer.similarplayers[0].name}</h2>
-            <p>IMAGE HERE</p>
-            <p>PG</p>
-            <p>6'5"</p>
-            <p>220lbs</p>
-            <p>College: Arizona State</p>
+            <img src={state.currentPlayer.similarplayers[0].imgurl} alt="similar player"></img>
+            <p>{state.currentPlayer.similarplayers[0].position}</p>
+            <p>{state.currentPlayer.similarplayers[0].height}</p>
+            <p>{state.currentPlayer.similarplayers[0].weight}</p>
+            <p>College:  {state.currentPlayer.similarplayers[0].college}</p>
           </div>
         </div>
         <div className="expected-years">
-          <p>PlayerName is expected to be in the NBA for Years years.</p>
+          <p> {state.currentPlayer.name} is expected to be in the NBA for {state.currentPlayer.prediction} years.</p>
         </div>
       </div>
     );
@@ -70,7 +71,7 @@ const PlayerList = () => {
           <SavedPlayer />
         </div>
         <div className="player-cards">
-          <p>Search for a player!</p>
+          <h2>Search for a player!</h2>
         </div>
       </div>
     );
