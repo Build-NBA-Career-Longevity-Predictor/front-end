4ca092c2d4517777755b277708fd357a2c856bc5
jambis <imjaamees@gmail.com>
2019-10-24 14:50:23 -0500

    Removed redux-logger

--- src/index.js
+++ src/index.js
@@ -4,11 +4,10 @@ import App from "./App";
 import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
-import logger from "redux-logger";
 import { reducer } from "./Reducers";
 import { SnackbarProvider } from "notistack";
 
-const store = createStore(reducer, applyMiddleware(thunk, logger));
+const store = createStore(reducer, applyMiddleware(thunk));
 
 ReactDOM.render(
   <Provider store={store}>
